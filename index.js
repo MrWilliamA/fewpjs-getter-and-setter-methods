43a0c89c756a993d9701188a3751601da6d88986
MrWilliamA <will.archer@hotmail.com>
2022-04-01 12:36:01 +1100

    progress

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your Circle class here
+
+class Circle {
+
+    constructor(radius){
+        this.radius = radius;
+        
+    }
+
+    
+    get diameter() {
+        return this.radius = this.radius * 2;
+    }
+
+    get circumference() {
+        const pie = Math.PI;
+        const diameter = this.diameter
+        const circumference = pie * diameter
+        console.log(pie * diameter);
+        return circumference;
+    }
+
+    get area() {
+        circle.area = area
+    }
+
+    set diameter(diameter) {
+        circle.diameter = diameter
+    }
+
+    set circumference(circumference) {
+        circle.circumference = circumference
+    }
+
+    set area(area) {
+        circle.area = area
+    }
+}
